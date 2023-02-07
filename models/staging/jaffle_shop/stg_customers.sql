b46fc3dbea4c926e168beb954a1c322eb44b5e46
trevor@infostrux.com <trevor@infostrux.com>
2023-02-07 22:47:29 +0000

    added source.yml for stripe and jaffle_shop

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,6 +1,17 @@
-select
+with 
+
+source as (
+
+    select * from {{ source('jaffle_shop','customers')}}
+), 
+
+staged as (
+    select
     id as customer_id,
     first_name,
     last_name
 
-from raw.jaffle_shop.customers
+from source
+)
+
+select * from staged
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,7 +1,17 @@
-select
+with source as (
+
+    select * from {{source('jaffle_shop','orders')}}
+),
+
+staged as (
+
+    select
     id as order_id,
     user_id as customer_id,
     order_date,
     status
 
-from raw.jaffle_shop.orders
+from source
+)
+
+select * from staged
