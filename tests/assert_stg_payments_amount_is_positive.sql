1f00a60685ec439e2fdb42b197dd9a903aafd80e
trevor@infostrux.com <trevor@infostrux.com>
2023-02-08 17:56:04 +0000

    add inline docs for src_jaffle_shop.yml

--- tests/assert_stg_payments_amount_is_positive.sql
+++ tests/assert_stg_payments_amount_is_positive.sql
@@ -1,3 +1,6 @@
+-- Refunds have a negative amount, so the total amount should always be >= 0
+-- Therefore return records where this isn't true to make this test fail.
+
 with payments as (
     select * from {{ ref('stg_payments') }}
 )
