b8f1154ae77f4698402c2de4c432bd2c247f5614
trevor@infostrux.com <trevor@infostrux.com>
2023-02-09 20:23:48 +0000

    add dev_days_of_data.sql macro

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,6 +4,8 @@ select
     paymentmethod as payment_method,
     status,
     -- amount is stored in cents, convert it to dollars
-    {{ cents_to_dollars('payment_amount', 4) }} as amount,
+    {{ cents_to_dollars('amount', 2) }} as amount,
     created as created_at
 from {{ source('stripe', 'payment') }}
+
+{{- limit_data_in_dev(column_name='created_at',dev_days_of_data=5) }}
